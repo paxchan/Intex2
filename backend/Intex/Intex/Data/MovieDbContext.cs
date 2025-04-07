d4c1658f7d03c0caeb622dae77600a90f699c5dd
Sarah Walker <sarahsweetpie6@gmail.com>
2025-04-07 16:12:06 -0600

    MoviesDb and models

---
+++ backend/Intex/Intex/Data/MovieDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Intex.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {   
+        }
+
+        public DbSet<movies_rating> MoviesRatings { get; set; }
+    }
+}
---
+++ backend/Intex/Intex/Data/movies_rating.cs
@@ -0,0 +1,9 @@
+﻿namespace Intex.Data
+{
+    public class movies_rating
+    {
+        public int user_id { get; set; }
+        public string show_id { get; set; }
+        public int rating { get; set; }
+    }
+}
