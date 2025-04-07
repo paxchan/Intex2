15f0366d8c0b8d7cd3cfe31cfe92419287635eef
connornebeker <connor.nebeker03@gmail.com>
2025-04-07 16:33:45 -0600

    controller stuff

--- backend/Intex/Intex/Controllers/MovieController.cs
+++ backend/Intex/Intex/Controllers/MovieController.cs
@@ -7,5 +7,40 @@ namespace Intex.Controllers
     [ApiController]
     public class MovieController : ControllerBase
     {
+
+        private MovieRecDbContext _movieContext;
+        public MovieController(MovieRecDbContext temp)
+        {
+            _movieContext = temp;
+        }
+
+        private UserRecDbContext _userContext;
+        public MovieController(UserRecDbContext temp)
+        {
+            _userContext = temp;
+        }
+
+        private UserLikedDbContext _userLikedContext;
+        public MovieController(UserLikedDbContext temp)
+        {
+            _userLikedContext = temp;
+        }
+    
+        [HttpGet("UserRec")]
+        public IActionResult GetUserRec(int userId)
+        {
+            var userRec = _movieContext.User_Recommendations
+                .Where(ur => ur.User == userId)
+                .ToList();
+
+            return Ok(userRec);
+        }
+
+        [HttpGet("MovieRec")]
+        public IActionResult GetMovieRec(string movieTitle){
+            var movieRec = _movieContext.Movie_Recommendations
+                .Where(mr => mr.original_title == movieTitle)
+                .ToList();
+        }
     }
 }
