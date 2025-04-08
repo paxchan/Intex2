3763241310d5a8169fc507d1ec108767c611b426
connornebeker <connor.nebeker03@gmail.com>
2025-04-08 10:10:29 -0600

    csp fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,11 +9,11 @@ export default defineConfig({
       'Content-Security-Policy':
         "default-src 'self'; " +
         "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
-        "style-src 'self' 'unsafe-inline'; " +
+        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
         "img-src 'self' data: https://image.tmdb.org; " +
         "frame-ancestors 'none'; " +
-        "font-src 'self' data:; " +
-        "connect-src 'self' https://localhost:5000, https://*.database.windows.net; " +
+        "font-src 'self' https://fonts.gstatic.com data:; " +
+        "connect-src 'self' https://localhost:5000 https://*.database.windows.net; " +
         "object-src 'none'; base-uri 'self'; form-action 'self';",
     },
   },
